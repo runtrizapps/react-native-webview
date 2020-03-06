c9a394dea3fbb7e22c6044900b280a106e4e0720
David Govea <govea.d@gmail.com>
2020-03-06 11:31:58 -0800

    set up app with 2 stack screens

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,13 +1,41 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { WebView } from 'react-native-webview';
+
+function MainScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('webview')}>
+        <Text>Launch Browser</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function WebViewScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'stretch' }}>
+      <WebView style={{flex: 1}} source={{ uri: 'https://reactnative.dev/' }} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={MainScreen} />
+        <Stack.Screen name="webview" component={WebViewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
